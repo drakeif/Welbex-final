a6e3b919b74fc409e2d1fcc9eb39d1d10f2072da
volodymyr <v.ohlopkov100@gmail.com>
2022-09-11 22:18:03 +0300

    v0.4
    11.09.22
    burger menu was added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-const burger = document.getElementById('burger');
+const burger = document.getElementById('burger'),
+      header = document.getElementById('header'),
+      nav = document.getElementById('nav'),
+      media = document.getElementById('media');
+      
 
 burger.addEventListener("click", (e) => {
     e.target.classList.toggle('active');
+    header.classList.toggle('active');
+    nav.classList.toggle('active');
+    media.classList.toggle('active');
 });
+
+closeMenu = document.querySelectorAll('.header__links');
+closeSocials = document.querySelectorAll('.header__socials');
+
+closeMenu.forEach(el => el.addEventListener('click', ()=> {
+    burger.classList.remove('active');
+    header.classList.remove('active');
+    nav.classList.remove('active');
+    media.classList.remove('active');
+  }));
+
+  closeSocials.forEach(el => el.addEventListener('click', ()=> {
+    burger.classList.remove('active');
+    header.classList.remove('active');
+    nav.classList.remove('active');
+    media.classList.remove('active');
+  }));
